8caa74e6fcb9b31d5c01e2d238b7d8d4c45b5ae8
hablu <131282586+hablu@users.noreply.github.com>
2024-03-26 16:41:44 +0600

    testimonial data create

---
+++ digital_agency/public/data/testimonial.ts
@@ -0,0 +1,19 @@
+export const testimonialData = [
+  {
+    image: "/image/testimonial/western-man-4975942-4159828 1.svg",
+    title: "Andrew Rathore",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. ",
+  },
+  {
+    image: "/image/testimonial/western-man-4975942-4159828 2.svg",
+    title: "Vera Duncan",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. ",
+  },
+  {
+    image: "/image/testimonial/western-man-4975942-4159828 3.svg",
+    title: "Mark Smith",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper scelerisque mi, in malesuada felis malesuada vel. ",
+  },
+];
+
+
